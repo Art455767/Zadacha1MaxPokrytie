221df97f205957e02e23bf2aa9731e3ee3eb0047
Artem Korolkov <opt.artem@bk.ru>
2024-06-12 16:46:44 +0300

    one commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+package ru.netology
+
+fun main() {
+    val transferAmount = 10000.0
+    val cardType = "Maestro"
+    val isUKRayTransfer = false
+
+    val commission = calculateCommission(transferAmount, cardType, isUKRayTransfer)
+    println("Комиссия за перевод: $commission рублей")
+}
+
+fun calculateCommission(transferAmount: Double, cardType: String, isUKRayTransfer: Boolean): Double {
+    val commissionRate = when (cardType) {
+        "Maestro" -> if (transferAmount in 1000.0..75000.0) 0.0 else 0.6 + 20
+        "Visa" -> if (isUKRayTransfer) 0.0 else 0.75
+        else -> throw IllegalArgumentException("Unsupported card type: $cardType")
+    }
+
+    return transferAmount * commissionRate / 100
+}
---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,63 @@
+package ru.netology
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+
+class MainKtTest {
+
+
+    @Test
+    fun `calculateCommission for Maestro card with transfer amount within range`() {
+        val transferAmount = 10000.0
+        val cardType = "Maestro"
+        val isUKRayTransfer = false
+
+        val commission = calculateCommission(transferAmount, cardType, isUKRayTransfer)
+
+        assertEquals(0.0, commission, 0.0)
+    }
+    @Test
+    fun `calculateCommission for Maestro card with transfer amount outside range`() {
+        val transferAmount = 100000.0
+        val cardType = "Maestro"
+        val isUKRayTransfer = false
+
+        val commission = calculateCommission(transferAmount, cardType, isUKRayTransfer)
+
+        assertEquals(80.0, commission, 0.0)
+    }
+
+
+    @Test
+    fun `calculateCommission for Visa card`() {
+        val transferAmount = 10000.0
+        val cardType = "Visa"
+        val isUKRayTransfer = false
+
+        val commission = calculateCommission(transferAmount, cardType, isUKRayTransfer)
+
+        assertEquals(75.0, commission, 0.0)
+    }
+
+    @Test
+    fun `calculateCommission for UK Ray transfer`() {
+        val transferAmount = 10000.0
+        val cardType = "Visa"
+        val isUKRayTransfer = true
+
+        val commission = calculateCommission(transferAmount, cardType, isUKRayTransfer)
+
+        assertEquals(0.0, commission, 0.0)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `calculateCommission for unknown card type`() {
+        val transferAmount = 10000.0
+        val cardType = "UnknownCard"
+        val isUKRayTransfer = false
+
+        calculateCommission(transferAmount, cardType, isUKRayTransfer)
+    }
+}
